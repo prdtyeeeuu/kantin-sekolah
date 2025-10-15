811a064fa688d13bfa2b2de906852550cb41b983
prdtyeeeuu <pradityadwi017@gmail.com>
2025-10-15 09:36:12 +0700

    pull oy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
     extend: {
+      colors: {
+        indigo: colors.indigo,
+      },
       backgroundImage: {
         'kantin-bg': "url('/images/bg.jpg')",
       }
